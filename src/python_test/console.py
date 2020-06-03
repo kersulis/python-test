d6fd48c3a7a13ebdc7b42f8de55b1c76fd1aae29
Jonas Kersulis <kersulis@umich.edu>
2020-06-03 15:38:47 -0400

    script for printing random wiki page summary with language option

--- src/python_test/console.py
+++ src/python_test/console.py
@@ -1,10 +1,25 @@
+import textwrap
+
 import click
+import requests
 
 from . import __version__
 
+
 @click.command()
 @click.version_option(version=__version__)
+@click.option("-l", "--language", default="en", help="Wikipedia language.")
 
-def main():
+def main(language):
     """Python test project."""
-    click.echo("Hello world")
+    API_URL = "https://" + language + ".wikipedia.org/api/rest_v1/page/random/summary"
+
+    with requests.get(API_URL) as response:
+        response.raise_for_status()
+        data = response.json()
+
+    title = data["title"]
+    extract = data["extract"]
+
+    click.secho(title, fg="green")
+    click.echo(textwrap.fill(extract))
