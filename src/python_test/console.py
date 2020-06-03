cc55da9fbb271b0d73b56d2c4b2c0f0d3f37413e
Jonas Kersulis <kersulis@umich.edu>
2020-06-03 16:56:42 -0400

    added end-to-end tests

--- src/python_test/console.py
+++ src/python_test/console.py
@@ -7,11 +7,17 @@ from . import __version__, wikipedia
 
 @click.command()
 @click.version_option(version=__version__)
-@click.option("-l", "--language", default="en", help="Wikipedia language.")
+@click.option(
+    "-l", "--language",
+    default="en",
+    help="Wikipedia language.",
+    metavar="LANG",
+    show_default=True
+)
 
 def main(language):
     """Python test project."""
-    data = wikipedia.random_page(language)
+    data = wikipedia.random_page(language=language)
 
     title = data["title"]
     extract = data["extract"]
