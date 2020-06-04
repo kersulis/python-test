4d40f8db64ad4261802a90a734c4ed7b1baf6343
Jonas Kersulis <kersulis@umich.edu>
2020-06-04 11:10:49 -0400

    build documentation with Sphinx

---
+++ docs/conf.py
@@ -0,0 +1,4 @@
+"""Sphinx configuration."""
+project = "python-test"
+author = "Jonas Kersulis"
+copyright = f"2020, {author}"
---
+++ docs/index.rst
@@ -0,0 +1,2 @@
+This is docs.index.rst,
+documenting the python test project.
