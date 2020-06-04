8b221819ef3258fd691be5c868a570245f26a4d6
Jonas Kersulis <kersulis@umich.edu>
2020-06-04 10:56:10 -0400

    add test docstrings

--- tests/test_console.py
+++ tests/test_console.py
@@ -20,21 +20,25 @@ def mock_wikipedia_random_page(mocker: MockFixture) -> Mock:
 
 
 def test_main_succeeds(runner: CliRunner, mock_requests_get: Mock) -> None:
+    """It exits with a status code of zero."""
     result = runner.invoke(console.main)
     assert result.exit_code == 0
 
 
 def test_main_prints_title(runner: CliRunner, mock_requests_get: Mock) -> None:
+    """It prints the Page title."""
     result = runner.invoke(console.main)
     assert "Lorem Ipsum" in result.output
 
 
 def test_main_invokes_requests_get(runner: CliRunner, mock_requests_get: Mock) -> None:
+    """It invokes requests.get."""
     runner.invoke(console.main)
     assert mock_requests_get.called
 
 
 def test_main_uses_en_wikipedia(runner: CliRunner, mock_requests_get: Mock) -> None:
+    """It uses English Wikipedia by default."""
     runner.invoke(console.main)
     args, _ = mock_requests_get.call_args
     assert "en.wikipedia.org" in args[0]
@@ -43,6 +47,7 @@ def test_main_uses_en_wikipedia(runner: CliRunner, mock_requests_get: Mock) -> N
 def test_main_fails_on_request_error(
     runner: CliRunner, mock_requests_get: Mock
 ) -> None:
+    """It exits with 1 when requests.get has an error."""
     mock_requests_get.side_effect = Exception("error")
     result = runner.invoke(console.main)
     assert result.exit_code == 1
@@ -51,6 +56,7 @@ def test_main_fails_on_request_error(
 def test_main_prints_message_on_request_error(
     runner: CliRunner, mock_requests_get: Mock
 ) -> None:
+    """It prints an error message when requests.get errors."""
     mock_requests_get.side_effect = requests.RequestException
     result = runner.invoke(console.main)
     assert "Error" in result.output
@@ -59,11 +65,13 @@ def test_main_prints_message_on_request_error(
 def test_main_uses_specified_language(
     runner: CliRunner, mock_wikipedia_random_page: Mock
 ) -> None:
+    """It uses the language passed with a given language code."""
     runner.invoke(console.main, ["--language=pl"])
     mock_wikipedia_random_page.assert_called_with(language="pl")
 
 
 @pytest.mark.e2e
 def test_main_succeeds_in_production_env(runner: CliRunner) -> None:
+    """It runs when it has access to the live Wikipedia API."""
     result = runner.invoke(console.main)
     assert result.exit_code == 0
--- tests/test_wikipedia.py
+++ tests/test_wikipedia.py
@@ -7,17 +7,20 @@ from python_test import wikipedia
 
 
 def test_random_page_uses_given_language(mock_requests_get: Mock) -> None:
+    """It uses the given language."""
     wikipedia.random_page(language="de")
     args, _ = mock_requests_get.call_args
     assert "de.wikipedia.org" in args[0]
 
 
 def test_random_page_returns_page(mock_requests_get: Mock) -> None:
+    """It returns a Page resource."""
     page = wikipedia.random_page()
     assert isinstance(page, wikipedia.Page)
 
 
 def test_random_page_handles_validation_errors(mock_requests_get: Mock) -> None:
+    """It handles validation errors."""
     mock_requests_get.return_value.__enter__.return_value.json.return_value = None
     with pytest.raises(click.ClickException):
         wikipedia.random_page()
